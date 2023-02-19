b76a77963ab83f0747461d7a0d9ad7ef9bd032b2
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-19 10:13:16 -0600

    added to files to navigate between

---
+++ src/components/PageOne.js
@@ -0,0 +1,10 @@
+const PageOne = () => {
+    return(
+        <div>
+            <div>hello from page one</div>
+            <button>Home Page</button>
+        </div>
+    )
+}
+
+export default PageOne;
---
+++ src/components/PageTwo.js
@@ -0,0 +1,10 @@
+const PageTwo = () => {
+    return(
+        <div>
+            <div>hello from page two</div>
+            <button>Home Page</button>
+        </div>
+    )
+}
+
+export default PageTwo;
