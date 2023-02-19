ebaa25e6a43483cc65636af538f7a8909b121efd
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-19 10:14:48 -0600

    added text to the home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Home Page</h1>
+      <button>Page One</button>
+      <button>Page Two</button>
     </div>
   );
 }
